ffcba7075490a97795d68c4a9df775506d49ea3a
yash-m-agarwal <yashmagarwal786@gmail.com>
2019-03-13 23:47:06 +0530

    Database Models Updated

--- models/forgotPassword.js
+++ models/forgotPassword.js
@@ -2,8 +2,9 @@ const Sequelize = require('sequelize');
 const sequelize = require('../config/dbConfig');
 
 const ForgotPassword = sequelize.define('forgotPassword', {
-    userId : { type: Sequelize.INTEGER, primaryKey: true},
-    OTP : { type: Sequelize.STRING(45), allowNull: false},
+    forgotPasswordId : { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
+    userId : { type: Sequelize.INTEGER, allowNull: false},
+    OTP : { type: Sequelize.STRING(45), allowNull: false}
 });
 
 module.exports = ForgotPassword;
--- models/transaction.js
+++ models/transaction.js
@@ -2,13 +2,15 @@ const Sequelize = require('sequelize');
 const sequelize = require('../config/dbConfig');
 
 const Transaction = sequelize.define('transaction', {
-    transactionId : { type: Sequelize.INTEGER, primaryKey: true },
+    transactionId : { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
     borrowerId : { type: Sequelize.INTEGER, allowNull: false },
+    borrowerName : { type: Sequelize.STRING(45), allowNull: false },
     lenderId : { type: Sequelize.INTEGER, allowNull: false },
+    lenderName : { type: Sequelize.STRING(45), allowNull: false },
     amount : { type: Sequelize.DOUBLE, allowNull: false, defaultValue: 0.0 },
-    requestedDate : { type: Sequelize.DATE, allowNull: false, defaultValue: Sequelize.NOW },
-    acceptedDate : { type: Sequelize.DATE, allowNull: false },
-    completionDate : { type: Sequelize.DATE, allowNull: false },
+    requestedDate : { type: Sequelize.DATEONLY, allowNull: false, defaultValue: Sequelize.NOW },
+    acceptedDate : { type: Sequelize.DATEONLY, allowNull: false },
+    completionDate : { type: Sequelize.DATEONLY, allowNull: false },
     status : { type: Sequelize.INTEGER, allowNull: false },
 });
 

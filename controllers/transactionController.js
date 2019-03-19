50ca06b42d5fb5a35faffcbcd5197b5f79640c96
yash-m-agarwal <yashmagarwal786@gmail.com>
2019-03-20 01:39:11 +0530

    Fixed transaction Controller

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -3,38 +3,24 @@ const transactionController = require('../models/transaction');
 const profile = require('../models/profile');
 const { response } = require('../helpers/response');
 
-
- var uniqid = require('uniqid');
- let id=uniqid();
-console.log(id);
-
 module.exports = {
     'sendRequest' : async (req,res,next) => {
         const {
-            //borrowerId,
             borrowerName,
             lenderId,
             lenderName,
-            amount,
-            requestedDate,
-            acceptedDate,
-            completionDate,
-            status
+            amount
         } = req.body;
 
         let borrowerId = req.userId;
 
         transactionController.create({
-            transactionId: id,
             borrowerId: borrowerId,
             borrowerName: borrowerName,
             lenderId: lenderId,
             lenderName: lenderName,
             amount: amount,
-            requestedDate: requestedDate,
-            acceptedDate: acceptedDate,
-            completionDate: completionDate,
-            status: status
+            status: 1
         }).then(ans=>{
             response(res, null, ans, null, 201);
         }).catch(err => {
